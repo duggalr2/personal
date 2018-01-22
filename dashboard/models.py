516077739aad66c77ad22b3361a4ce63e4d2dd0e
Rahul Duggal <ibrahul24@gmail.com>
2018-01-22 15:48:22 -0500

    created models

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    book_name = models.CharField(max_length=500)
+
+
+class Project(models.Model):
+    project_name = models.CharField(max_length=500)
+    project_description = models.TextField()
+
+
+class Course(models.Model):
+    course_name = models.CharField(max_length=500)
+
+
+# class Schedule(models.Model):
+#     book_name = models.CharField(max_length=500)
+#
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.forms import PasswordChangeForm
+import ast
+from django.contrib.auth.decorators import user_passes_test
+from django.conf import settings
 
+
+def home(request):
+    return render(request, 'home.html')
 
