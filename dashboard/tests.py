8788e76328bd2d62963f4b69546d269379402f13
Rahul Duggal <ibrahul24@gmail.com>
2018-01-23 15:25:48 -0500

    writing some unittests

--- dashboard/tests.py
+++ dashboard/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from .models import Book
+from .forms import BookForm
 
-# Create your tests here.
+
+# models test
+class BookTest(TestCase):
+
+    def create_book(self, title="only a test"):
+        return Book.objects.create(book_name=title)
+
+    def test_book_creation(self):
+        w = self.create_book()
+        self.assertTrue(isinstance(w, Book))
+        self.assertEqual(w.book_name, 'only a test')
+
+    def test_valid_form(self):
+        w = Book.objects.create(book_name='Foo')
+        data = {'book_name': w.book_name}
+        form = BookForm(data=data)
+        self.assertTrue(form.is_valid())
+
+    def test_invalid_form(self):
+        w = Book.objects.create(book_name='')
+        data = {'book_name': w.book_name}
+        form = BookForm(data=data)
+        self.assertFalse(form.is_valid())
--- personal/settings.py
+++ personal/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'dashboard',
+    'coverage',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
